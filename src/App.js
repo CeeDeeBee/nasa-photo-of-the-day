2592a163653e0c2a181708129d80e311a26e59d5
CeeDeeBee <cdbarnes98@gmail.com>
2020-02-19 23:11:46 -0500

    Added date picker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ApodContainer from "./components/ApodContainer/ApodContainer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ApodContainer />
+      <Footer />
     </div>
   );
 }
--- src/components/ApodContainer/ApodContainer.jsx
+++ src/components/ApodContainer/ApodContainer.jsx
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Title from "./Title";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import Image from "./Image";
 import Explanation from "./Explanation";
 
 const ApodContainer = () => {
     const [apodData, setApodData] = useState({});
+    const [selectedDate, setSelectedDate] = useState(new Date());
 
     useEffect(() => {
-        console.log('test');
-        axios.get("https://api.nasa.gov/planetary/apod?api_key=kS5WZiKNXcbuoFjmFbmgL6KkwjVl3xxSpxUUc31H")
+        const day = selectedDate.getDate();
+        const month = selectedDate.getMonth() + 1;
+        const year = selectedDate.getFullYear();
+        axios
+            .get(`https://api.nasa.gov/planetary/apod?date=${year}-${month}-${day}&api_key=kS5WZiKNXcbuoFjmFbmgL6KkwjVl3xxSpxUUc31H`)
             .then(res => setApodData(res.data))
             .catch(err => console.log(err));
-    }, []);
+    }, [selectedDate]);
 
     return (
         <section className="apod-container">
             <Title title={apodData.title} />
+            <DatePicker selected={selectedDate} onChange={date => setSelectedDate(date)} />
             <Image url={apodData.url} />
             <Explanation explanation={apodData.explanation} />
         </section>
